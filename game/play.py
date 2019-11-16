4ebd4ce6852fd202cc37410ad2f26da660a73f13
gabriel-08 <gabewang2008@gmail.com>
2019-11-15 20:23:52 -0800

    Game
    A small sim game.

---
+++ game/play.py
@@ -0,0 +1,15 @@
+print('You are a slave in Virginia. Your master gives you shovel. What do you do? \n A)You obey  B)You hit him')
+a = input()
+print("You chose %s" % a)
+if a == "A" or a == "a":
+    b = input('You work hard until it is dark. Your master calls you in. Do you obey? \n A)Yes  B)No \n')
+    if b == "A" or b == "a":
+        c = input("Your master sells you to a cruel owner. What do you do? \n A)You run  B)You obey \n")
+        if c == "A" or c =="a":
+            print("Your new master sends his dogs at you, and you get killed. REPLAY the game! ")
+        elif c == "B" or c == "b":
+            print("The first thing your master does is whip you. You bleed to death. REPLAY the game!")
+    elif b == "B" or b == "b":
+        print("You successfully run away. Harriet Tubman finds you. You become a free man.YAY!")
+elif a == "B" or a == "b":
+    print("The watcher whips you! You are confined to a bed for 2 weeks. You are starved to death. REPLAY the game!")
